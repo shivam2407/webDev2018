17cca0638ea379b534c9a53c89531d67bc00248b
Javesh <shivarawat24@gmail.com>
2018-06-10 16:22:41 -0400

    Added cross origin aceptance

--- src/main/java/com/example/webdev/services/CourseService.java
+++ src/main/java/com/example/webdev/services/CourseService.java
@@ -1,6 +1,7 @@
 package com.example.webdev.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import com.example.webdev.modals.Course;
 import com.example.webdev.repositories.CourseRepository;
 
 @RestController
+@CrossOrigin(origins = "*",maxAge = 3600)
 public class CourseService {
 	
 	@Autowired
